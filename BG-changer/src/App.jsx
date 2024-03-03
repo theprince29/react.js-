748cd22cf7c7d8392fd180e0955a6981f81fba17
Prince_Pal <princeofbanarasup65@gmail.com>
2024-03-03 16:05:48 +0530

    Done BG-changer

--- BG-changer/src/App.jsx
+++ BG-changer/src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import "././App.css"
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setcolor] = useState("olive");
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div
+        className="w-full h-screen duration-200"
+        style={{   
+          background: color,
+          backgroundSize: '400% 400%',
+          animation: 'gradientAnimation 3s infinite alternate',}}
+      ></div>
+      <div className="fixed flex flex-wrap justify-center top-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-sm">
+
+          <button
+            onClick={() => {
+              setcolor("linear-gradient(-45deg,#ae2012, #ff0000,#e63946)");
+            }}
+            className="outline-none px-4 rounded-sm text-white shadow-lg"
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
+
+          <button
+            onClick={() => {
+              setcolor("linear-gradient(-45deg,  #00FF00,#1b9200,	#007c15)");
+            }}
+            className="outline-none px-4 rounded-sm text-white shadow-lg"
+            style={{ backgroundColor: "green" }}
+          >
+            Green
+          </button>
+
+          <button
+            onClick={() => {setcolor("linear-gradient(-45deg, #003366, #3399CC, #66CCFF)");}
+          }
+            className="outline-none px-4 rounded-sm text-white shadow-lg"
+            style={{ backgroundColor: "blue" }}
+          >
+            Blue
+          </button>
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
